880fced82195886f61d4bc4bae627e7851a528b4
jmanzanares12 <jimgken2004@gmail.com>
2025-01-10 16:49:49 -0600

    Add: footer and new file pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeaderApp from './components/headerApp'
 import AboutUsApp from './components/aboutUsApp'
+import FooterApp from './components/footerApp'
 
 function App() {
   return (
     <div>
       <HeaderApp />
       <AboutUsApp />
+      <FooterApp/>
     </div>
   )
 }
---
+++ src/components/footerApp.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function FooterApp() {
+    return(
+        <>
+            <footer className="footerApp">
+                <div className="container">
+                    <h3>Hola soy un footer</h3>
+                </div>
+            </footer>
+        </>
+    )   
+}
+
+export default FooterApp;
