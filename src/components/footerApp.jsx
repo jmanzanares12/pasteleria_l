8034d5913f88d987fa94a9ceb453f32acdd40f7d
jmanzanares12 <jimgken2004@gmail.com>
2025-01-10 23:23:39 -0600

    Add: new elements to footer

--- src/components/footerApp.jsx
+++ src/components/footerApp.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import '../styles/footerApp.css';
+import logo from '../images/logo.svg'
 
 function FooterApp() {
     return(
         <>
             <footer className="footerApp">
-                <div className="container">
-                    <h3>Hola soy un footer</h3>
+                <div className="imgApp">
+                    <img src={logo} alt="logo" />
                 </div>
+                <div className="addressApp">
+                    <ul>
+                        <li>Barrio Camilo Ortega II Etapa, Frente al Museo Camilo Ortega, Masaya.</li>
+                        <li>Barrio Camilo Ortega II Etapa, Del Museo Camilo Ortega 25 metros al este, Masaya.</li>
+                        <li>Mercado Municipal Ernesto Fernandez, Sector Pastelero, Masaya</li>
+                        <li>Calle Atravezada, Frente a Financiera Fama, Granada</li>
+                        <li>Entrada Principal de Diriomo, 50 metros al este, Diriomo.</li>
+                    </ul>
+                </div>
+
+
             </footer>
         </>
     )   
