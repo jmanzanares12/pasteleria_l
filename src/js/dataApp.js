4323eb9ca90eb1705db3ae7842f9613365bd9a78
jmanzanares12 <jimgken2004@gmail.com>
2025-01-17 10:33:48 -0600

    Change: img address for new url

--- src/js/dataApp.js
+++ src/js/dataApp.js
@@ -1,15 +1,15 @@
 const data = [
     {
         id: 0, 
-        imgURL: "src/assets/images/Masaya.svg"
+        imgURL: "https://nicasnews.com/wp-content/uploads/2016/09/18390902-2.jpg"
     },
     {
         id: 1, 
-        imgURL: "src/assets/images/Granada.svg"
+        imgURL: "https://media.istockphoto.com/id/155350183/es/foto/catedral-de-granada-nicaragua.jpg?s=612x612&w=0&k=20&c=IaixX13cycx4ZsHdb3kQ2RRn94ZmuBUvbB-xt6R8_PQ="
     },
     {
         id: 2, 
-        imgURL: "src/assets/images/Diriomo.svg"
+        imgURL: "https://www.mined.gob.ni/mapatradicionesculturapopular/wp-content/uploads/2022/02/diriomo-mode-101.png"
     }
 ]
 
