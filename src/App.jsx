5d992480accb90b1b65f2333cd52227d6485833f
jmanzanares12 <jimgken2004@gmail.com>
2025-01-20 15:27:45 -0600

    Add: new dependences and fix errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import HeaderApp from './components/headerApp'
-import AboutUsApp from './components/aboutUsApp'
-import FooterApp from './components/footerApp'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HeaderApp from './components/headerApp';
+import AboutUsApp from './components/aboutUsApp';
+import FooterApp from './components/footerApp';
 
 function App() {
   return (
-    <div>
-      <HeaderApp />
-      <AboutUsApp />
-      <FooterApp/>
-    </div>
-  )
+    <Router>
+        <HeaderApp />
+        <Routes>
+          <Route path="/" element={<AboutUsApp />} />
+          <Route path="/src/components/aboutUsApp.jsx" element={<AboutUsApp />} />
+          <Route path="/productos" element={<h1>Productos</h1>} />
+          <Route path="/contactanos" element={<h1>Contáctanos</h1>} />
+        </Routes>
+        <FooterApp />
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/components/headerApp.jsx
+++ src/components/headerApp.jsx
@@ -1,24 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../assets/images/logo.svg";
 import '../styles/headerApp.css';
 
 function HeaderApp() {
     return (
-        <>
-            <header className="headerApp">
-                <div className="logo">
-                    <img src={logo} alt="Logo" />
-                </div>
-                <div className="content">
-                    <ul>
-                        <li><a href="">Quienes somos</a></li>
-                        <li><a href="">Productos</a></li>
-                        <li><a href="">Contactanos</a></li>
-                    </ul>
-                </div>
-            </header>
-        </>
-    )
+        <header className="headerApp">
+            <div className="logo">
+                <img src={logo} alt="Logo" />
+            </div>
+            <div className="content">
+                <ul>
+                    <li><Link to="../components/aboutUsApp.jsx">Quienes somos</Link></li>
+                    <li><Link to="/productos">Productos</Link></li>
+                    <li><Link to="/contactanos">Contáctanos</Link></li>
+                </ul>
+            </div>
+        </header>
+    );
 }
 
-export default HeaderApp
+export default HeaderApp;
