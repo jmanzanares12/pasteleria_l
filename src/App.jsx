5eb82f065ec39ff62d7318dca9a11a4d06f90ac7
jmanzanares12 <jimgken2004@gmail.com>
2025-03-21 20:46:00 -0600

    Add: tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 function App() {
   return (
-    <>
-    </>
-  );
+    <div className='bg-slate-100 p-20'>
+      <h1>Hello Vite!</h1>
+    </div>
+  )
 }
 
 export default App;
