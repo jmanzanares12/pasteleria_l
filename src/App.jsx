6f4680f4a442c5dfe8ff1f69b53dc99202fef338
jmanzanares12 <jimgken2004@gmail.com>
2025-03-21 20:37:40 -0600

    Delete: all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HeaderApp from './components/headerApp';
-import FooterApp from './components/footerApp';
-import HomeApp from './pages/homeApp';
-import ProductApp from './pages/productApp';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route
-          path='/pasteleria_l'
-          element={
-            <>
-              <HeaderApp />
-              <HomeApp />
-              <FooterApp />
-            </>
-          } />
-
-        <Route
-          path='/productos'
-          element={
-            <>
-              <HeaderApp />
-              <ProductApp />
-              <FooterApp />
-            </>
-          } />
-      </Routes>
-    </Router>
+    <>
+    </>
   );
 }
 
