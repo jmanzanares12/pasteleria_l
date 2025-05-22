827485ab4cbc9c464c0ffd7b6c2f37cb8b11203b
jmanzanares12 <jimgken2004@gmail.com>
2025-05-22 00:27:53 -0600

    fix: error

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import Providers from "./Providers";
 import AppRoutes from "./routes";
 import Header from "./components/header";
 import Footer from "./components/footer";
 
 const App = () => {
   return(
-    <Providers>
+    <>
       <Header />
       <main className="min-h-screen">
         <AppRoutes />
       </main>
       <Footer />
-    </Providers>
+    </>
   )
 }
 
