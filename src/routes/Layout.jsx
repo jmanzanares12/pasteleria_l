5af56a6b361805566ddbbad1c36c9699c12f5229
jmanzanares12 <jimgken2004@gmail.com>
2026-03-13 17:35:49 -0600

    Fix: correct case sensitivity in Header and Footer component imports

--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Header from '../components/Header.jsx';
-import Footer from "../components/Footer.jsx";
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 export default function Layout() {
     return (
